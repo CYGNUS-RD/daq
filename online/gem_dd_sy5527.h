7563ced96425c60a7aebc8db5a4d7aece8ab96ce
piacent <stefano.piacentini@hotmail.com>
2025-07-21 17:28:26 +0200

    new sy5527 HV module device driver; possible solution to memory leak

---
+++ online/gem_dd_sy5527.h
@@ -0,0 +1,12 @@
+/********************************************************************\
+
+  Name:         dd_sy5527.h
+  Created by:   Stefano Piacentini, based on dd_sy4527 by Stefan Ritt
+
+  Contents:     Device driver function declarations for SY5527 device
+
+  $Id$
+
+\********************************************************************/
+
+INT dd_sy5527(INT cmd, ...);
